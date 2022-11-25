ba6925a4db75a9d67ac576d6293774645b4b7601
NHuyHoangg <80319763+NHuyHoangg@users.noreply.github.com>
2022-11-25 16:39:18 +0700

    change product api

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -7,38 +7,37 @@ const { pool } = require("../database/dbinfo");
 
 module.exports = {
   get: (req, res) => {
-    let sql = (
-        'SELECT product_product.sku '+
-        ', product_product.id ' + 
-        '  , product_product.name AS product_name'+
-      '    , product_product.price '+
-      '    , product_product.discount_price'+
-        ' , product_collection.name AS collection_name '+
-      '   , product_category.name AS category_name '+
-      '   , image.content AS product_image '+
-      ' FROM product_product '+
-      ' LEFT JOIN product_collection '+
-        ' ON product_product.collection_id = product_collection.id '+
-      ' LEFT JOIN product_category '+
-      '   ON product_product.category_id = product_category.id '+
-      ' LEFT JOIN ( '+
-                  '  SELECT a.id AS id '+
-                  '    , a.content '+
-                  '    , a.product_id AS product_id '+
-                  'FROM product_media as a '+
-                  'WHERE a.id = ( '+
-                  'SELECT b.id '+
-                  'FROM product_media as b '+
-                  'WHERE a.product_id = b.product_id '+
-                  'ORDER BY b.id ASC '+
-                  'LIMIT 1)) AS image'+
-                  "          "+
-      '   ON product_product.id = image.product_id;'
-    );
-    console.log (sql);
+    let sql =
+      "SELECT product_product.sku " +
+      ", product_product.id " +
+      "  , product_product.name AS product_name" +
+      "    , product_product.price " +
+      "    , product_product.discount_price" +
+      " , product_collection.name AS collection_name " +
+      "   , product_category.name AS category_name " +
+      "   , image.content AS product_image " +
+      " FROM product_product " +
+      " LEFT JOIN product_collection " +
+      " ON product_product.collection_id = product_collection.id " +
+      " LEFT JOIN product_category " +
+      "   ON product_product.category_id = product_category.id " +
+      " LEFT JOIN ( " +
+      "  SELECT a.id AS id " +
+      "    , a.content " +
+      "    , a.product_id AS product_id " +
+      "FROM product_media as a " +
+      "WHERE a.id = ( " +
+      "SELECT b.id " +
+      "FROM product_media as b " +
+      "WHERE a.product_id = b.product_id " +
+      "ORDER BY b.id ASC " +
+      "LIMIT 1)) AS image" +
+      "          " +
+      "   ON product_product.id = image.product_id;";
+    console.log(sql);
     pool.query(sql, (err, response) => {
       if (err) throw err;
       res.json(response);
     });
-  }
+  },
 };
--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const {pool} =  require('./database/dbinfo');
+const { pool } = require("./database/dbinfo");
 const app = express();
-//  const productsCtrl = require("./controller/ProductController");
+const productsCtrl = require("./controller/ProductController");
 dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res)=>{
-    res.send('i love u');
-})
+app.get("/", (req, res) => {
+  res.send("i love u");
+});
 
 // test connect
-app.use('/server/test', require('./test'));
+app.use("/server/test", require("./test"));
 
 //api route
 /**
     Phân route theo từng nhóm
 **/
 
-// app.route("/products")
-//     .get(productsCtrl.get)
+app.route("/products").get(productsCtrl.get);
 
 // app.route("/products/:productId")
 //     .get(productsCtrl.detail)
 
-app.get("/products", (req, res) => {
-   let sql = "SELECT * FROM product_product";
-   pool.query(sql, (err, response) => {
-     if (err) throw err;
-     res.json(response);
-   });
-});
 //auth
 app.use("/server/auth", require("./controller/AuthController"));
 //product
 
 //
 
-
-app.listen("3001", ()=>{
-    console.log('server started running on 3001');
-})
-
+app.listen("3001", () => {
+  console.log("server started running on 3001");
+});
