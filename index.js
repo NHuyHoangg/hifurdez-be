b8cbe95ecbfb53665395466959d3ed461256ba6e
NHuyHoangg <80319763+NHuyHoangg@users.noreply.github.com>
2022-11-27 23:43:22 +0700

    add cors

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const { pool } = require("./database/dbinfo");
 const app = express();
+
+const cors = require('cors');
+
 const productsCtrl = require("./controller/ProductController");
 dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+// app.use(cors({ origin: "https://www.hifurdez.studio/" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("<html> <img src='https://ik.imagekit.io/amnd3xdhd/316166119_526826592671589_9115068966916421847_n.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669486983800' style='height: 100%'></html>");
