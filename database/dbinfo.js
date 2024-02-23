412d933e8c24899b39df30f0ad06f8f328bc2806
Deltora <deltora1st@gmail.com>
2024-02-23 16:50:49 +0700

    config rds db

--- database/dbinfo.js
+++ database/dbinfo.js
@@ -2,7 +2,13 @@ const mysql = require("mysql2");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
-const pool = new mysql.createConnection(process.env.DATABASE_URL);
+var pool = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
+});
 
 module.exports = {
   pool,
